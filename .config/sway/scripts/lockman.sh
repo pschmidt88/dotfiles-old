3830c87f0af279f198d98de7d1f900224ac8511c
Paul Schmidt <ps@fino.digital>
2023-02-19 10:55:17 +0100

    Add nwg bar und some updates

--- .config/sway/scripts/lockman.sh
+++ .config/sway/scripts/lockman.sh
@@ -3,6 +3,10 @@
 swayidle \
     timeout  5 'swaymsg "output * dpms off"' \
     resume 'swaymsg "output * dpms on"' &
+
+# pause music before locking screen
+playerctl -a pause
+
 # Locks the screen immediately
 swaylock -f -C ~/.config/swaylock/config
 # Kills last background task so idle timer doesn't keep running
